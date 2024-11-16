f8713fadf9684310cdf8637faf2dc5f6fcf83329
Robert Beres <77006934+Robert076@users.noreply.github.com>
2024-11-16 18:38:57 +0200

    made sure the game resets on correct guess

--- server.py
+++ server.py
@@ -20,7 +20,6 @@ def worker(cs):
         if num == serverNumber and notGuessed:
             mylock.acquire()
             winnerThread = threading.get_ident()
-            e.set()
             notGuessed = False
             mylock.release()
         cs.sendall(b'C')
@@ -28,18 +27,38 @@ def worker(cs):
     if winnerThread == threading.get_ident():
         print(f"Thread {curCount} has won after {curTries} try(tries)")
         cs.sendall(b'W')
+        e.set()
     else:
         cs.sendall(b'L')
     cs.close()
+    print(f"Worker thread {curCount} stopped")
 
-def resetSrv(cs):
-    pass
+
+def resetSrv():
+    global threads, serverNumber, clientCount, mylock, e, notGuessed
+    while True:
+        e.wait()
+        for t in threads:
+            t.join()
+
+        print("All threads have finished")
+        e.clear()
+        mylock.acquire()
+        threads = []
+        serverNumber = random.randint(0, 10)
+        clientCount = 0
+        notGuessed = True
+        winnerThread = -1
+        mylock.release()
+
 
 if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('0.0.0.0', 1234))
     s.listen(5)
     print("Server started...")
+    t=threading.Thread(target=resetSrv, daemon=True)
+    t.start()
     while True:
         clientSocket, clientAddress = s.accept()
         print(f'New connection from {clientAddress[0]}:{clientAddress[1]}')
